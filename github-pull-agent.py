b0dcaaa87ecbde3dfedf683c03854a6206bffa3b
Alexander Bezzubov <abezzubov@nflabs.com>
2014-03-15 19:00:16 +0900

    credentials logic updated

--- github-pull-agent.py
+++ github-pull-agent.py
@@ -3,9 +3,9 @@
 from __future__ import print_function
 
 __author__ = 'abezzubov@nflabs.com (Alex Bzz)'
-__copyright__  = 'Copyright 2014 NFLabs Inc'
+__copyright__  = 'Copyright 2014'
 
-"""Python agent to pull list of repo using plain Github3 API
+"""Python agent to pull list of repositories using plain Github3 API
 """
 
 import sys
@@ -20,21 +20,38 @@ class VanillaGithub(object):
     Very simple wrapper aroung GithubAPI3, supports only token auth method
     """
 
-    def __init__(self, login_or_token=None):
-        self.__oauth_token = login_or_token
-        self.__auth_headers = {'f8df3fbbf54e60d92ff55cc61ee4ebf0df91c68d':'x-oauth-basic'}
+    def __init__(self, token=None):
+        self.__oauth_token = token
+        self.__oauth_headers = (self.__oauth_token, 'x-oauth-basic')
         self.__rate_limit = 5000
 
     def get_rate_limit(self):
         return self.__rate_limit
 
-    def get_repos(self):
-        return self.get_repos_from_url('/'.join([DEFAULT_BASE_URL, 'repositories']))
+    def get_repos(self, first=0 , last=None):
+        """Iterates over public repositorise in (first, last] range.
 
-    def get_repos_from_url(self, url):
+        Actual repo list is a superset of the given range i.e (firest, last, ....] 
+        Args:
+            fist_repo: id of repository to start interation from
+            last_repo: id of repository to end interation from
+        """
+        repo_url = 'repositories'
+        if first > 0:
+            repo_url += '?since={}'.format(first) 
+        return self.get_repos_from_url("https://api.github.com/{}".format(repo_url), last)
+
+    def get_repos_from_url(self, url, last):
+        """Generator to fetch repositories from the url.
+
+        last repository id is agranteed to be included in the result
+        Args:
+            url: github API url to fetch from
+            last: id of repository to fetch up to
+        """
         number_of_repos = 0
         while True:
-            r = requests.get(url, auth=('f8df3fbbf54e60d92ff55cc61ee4ebf0df91c68d','x-oauth-basic'))
+            r = requests.get(url, auth=self.__oauth_headers)
             if (r.ok):
                 repoItems = r.json()
                 if len(repoItems) == 0:
@@ -47,7 +64,8 @@ class VanillaGithub(object):
                 yield repoItems
                 print(url)
             else:
-                print(r)
+                print(r.json())
+                return
 
     def parse_link(self, link_header):
         url = link_header.split(',')[0].split(';')[0][1:-1]
@@ -56,7 +74,7 @@ class VanillaGithub(object):
         
 
 def main():
-    g = VanillaGithub("f7f644d423f90dd2962961272c888b37c33bb8c9")
+    g = VanillaGithub("f8df3fbbf54e60d92ff55cc61ee4ebf0df91c68d")
     
     with open('raw-github-repos.txt', 'wb+') as f: 
         for repo in g.get_repos():
